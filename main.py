71b0e138c259c399e2f015ad9dc22b655b0fa430
Arthur LAUNOY <alaunoy@consultants-solutec.fr>
2023-05-02 14:34:08 +0200

    SPM-41 every week the user has now multiple options

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from spacemart import SpaceMart
-import argparse
+import argparse, pyinputplus as pyip
 
-def user_interaction(budget):
-    x = input(f"Remaining budget: {budget}.\nDo you want to continue? (Y/N)")
-    return (x == 'Y') 
-
-def init(budget):
+def user_interaction(mart):
+    result = pyip.inputMenu(['Continue', 'Give up', 'Fast forward'], "What do you want to do?\n", numbered=True)
+    match result:
+        case 'Continue':
+            return (True, 0)
+        case 'Give up':
+            return (False, 0)
+        case 'Fast forward':
+            result2 = pyip.inputNum(prompt="How many days in the future do you want to fast forward to?")
+            return (True, result2)
+        case _:
+            pass
+    
+def init(mart):
     print("This is the beginning of the program.")
-    print(f"Your total budget is: {budget}.")
+    print(f"Your total budget is: {mart.get_budget()}.")
     print("Good luck!")
+    print("-------------------------------------")
 
 def main_loop(mart):
-    init(mart.get_budget())
+    init(mart)
     while True:
         days = mart.get_time_passed() # updating time passed
         budget = mart.get_budget() # updating current budget
@@ -20,8 +30,13 @@ def main_loop(mart):
 
         if days % 7 == 0: # interact with user every week
             print(f"This is day {days}.")
-            if not user_interaction(budget):
+            cont, ff =  user_interaction(mart)
+            if not cont:
                 break
+            else:
+                if ff != 0:
+                    mart.add_time(ff)
+                    continue
         if days % 30 == 0: # pay taxes every month
             txt = "1 month has passed" if days == 30 else f"{int(days / 30)} months have passed"
             print(txt)
