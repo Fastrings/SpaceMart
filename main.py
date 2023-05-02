05dca16ed63c90dbe7487a87bfa737893e22a192
Arthur LAUNOY <alaunoy@consultants-solutec.fr>
2023-05-02 09:52:07 +0200

    SPM-25 program now stop when budget reaches 0. every month, some moeny is taken off of the budget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def user_interaction():
-    x = input("Do you want to continue? (Y/N)")
+def user_interaction(budget):
+    x = input(f"Remaining budget: {budget}.\nDo you want to continue? (Y/N)")
     return (x == 'Y') 
 
 def init(budget):
@@ -11,11 +11,19 @@ def main_loop(budget):
     days = 1
     init(budget)
     while True:
+        if budget < 0:
+            print("You are out of money. Better luck next time!")
+            break
         if days % 7 == 0:
             print(f"This is day {days}.")
-            if not user_interaction():
+            if not user_interaction(budget):
                 break
+        if days % 30 == 0:
+            txt = "1 month has passed" if days == 30 else f"{int(days / 30)} months have passed"
+            print(txt)
+            print("You pay 30 space dollars in taxes.")
+            budget -= 30
         days += 1
 
 if __name__ == '__main__':
-    main_loop(10000)
+    main_loop(100)
