3268404e4042c12ea025d5a6e1c71195e6e6b2eb
Arthur LAUNOY <alaunoy@consultants-solutec.fr>
2023-05-09 17:34:23 +0200

    added quantity attribute to products in current inventory. file is now closed

--- json_interface.py
+++ json_interface.py
@@ -4,4 +4,7 @@ def get_starting_inventory():
     file = open('products.json', 'r')
     data = json.load(file)['products']
     sam = random.sample(data, 200)
+    for product in sam:
+        product['quantity'] = 5
+    file.close()
     return sam
