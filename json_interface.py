fdb493347bd7eca3bfde31567d34b4ce137169d4
Arthur LAUNOY <alaunoy@consultants-solutec.fr>
2023-05-17 09:54:09 +0200

    using with keyword to close file automatically

--- json_interface.py
+++ json_interface.py
@@ -1,12 +1,11 @@
 import random, json
 
 def get_starting_inventory():
-    file = open('products.json', 'r')
-    data = json.load(file)['products']
-    sam = random.sample(data, 200)
-    for product in sam:
-        product['quantity'] = 5
-        product['discount'] = 0
-        product['remaining_days'] = random.randint(5, 25)
-    file.close()
+    with open('products.json', 'r') as file:
+        data = json.load(file)['products']
+        sam = random.sample(data, 200)
+        for product in sam:
+            product['quantity'] = 5
+            product['discount'] = 0
+            product['remaining_days'] = random.randint(5, 25)
     return sam
